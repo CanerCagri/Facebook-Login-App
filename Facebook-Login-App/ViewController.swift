fa0855bfcd3db37e569aa01960fe60487b7db081
CanerCagri <canerrcagrii@gmail.com>
2022-08-20 18:35:32 +0300

    Custom facebook login button created.

--- Facebook-Login-App/ViewController.swift
+++ Facebook-Login-App/ViewController.swift
@@ -8,47 +8,67 @@ class ViewController: UIViewController, LoginButtonDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let token = AccessToken.current,
-           !token.isExpired {
-            let token = token.tokenString
-            
-            let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
-                                                     parameters: ["fields" : "email, name"],
-                                                     tokenString: token,
-                                                     version: nil,
-                                                     httpMethod: .get)
-            
-            request.start { connection, result, error in
-                print(result)
-            }
-        } else {
-            let loginButton = FBLoginButton()
-            loginButton.center = view.center
-            loginButton.delegate = self
-            loginButton.permissions = ["public_profile", "email"]
-            view.addSubview(loginButton)
-        }
+        let loginButton = FBLoginButton()
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y: 100, width: view.frame.width - 32, height: 50)
+        loginButton.delegate = self
+        loginButton.permissions = ["public_profile", "email"]
         
+        let customButton = UIButton(type: .system)
+        view.addSubview(customButton)
+        customButton.backgroundColor = .systemPink
+        customButton.setTitle("Custom Facebook Button", for: .normal)
+        customButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        customButton.setTitleColor(.systemBackground, for: .normal)
+        customButton.frame = CGRect(x: 16, y: 200, width: view.frame.width - 32, height: 50)
+        
+        customButton.addTarget(self, action: #selector(handleCustomButton), for: .touchUpInside)
         
     }
     
-    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
-        let token = result?.token?.tokenString
+    @objc func handleCustomButton() {
+        LoginManager().logIn(permissions: ["public_profile", "email"], from: self) { result, error in
+            if error != nil {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            self.showEmailAdres(token: result?.token?.tokenString)
+        }
+    }
+    
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?){
         
+        if error != nil {
+            print(error!.localizedDescription)
+            return
+        }
+        
+       
+        
+        showEmailAdres(token: result?.token?.tokenString)
+      
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("Did logout")
+    }
+    
+    func showEmailAdres(token: String?) {
         let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
-                                                 parameters: ["fields" : "email, name"],
+                                                 parameters: ["fields" : "id, email, name"],
                                                  tokenString: token,
                                                  version: nil,
                                                  httpMethod: .get)
         
         request.start { connection, result, error in
+            if error != nil {
+                print("Failed to start GraphRequest")
+                return
+            }
+            
             print(result)
         }
-        
     }
-    
-    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
-        
-    }
-    
 }
